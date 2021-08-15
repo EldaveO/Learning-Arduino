70a30771914d64c2b4973de1e14d52d88ac6b0de
David Oblinsky <63413920+EldaveO@users.noreply.github.com>
2021-08-15 16:59:46 -0600

    Servo now spins without pot
    making the servo motor spin forwards and then completely reset when it reaches the end of it's movement (179 degrees)

--- Project05/Project05.ino
+++ Project05/Project05.ino
@@ -4,23 +4,35 @@ Servo MyServo;
 
 // Declare Variables
 int const POT_PIN = A0;
-int PotVal;
+//int PotVal;
+int RotVal;
 int Angle;
 
 void setup()
 {
  MyServo.attach(9);
- Serial.begin(9600); 
+ Serial.begin(9600);
+ RotVal = 0; 
 }
 
 void loop() 
 {
- PotVal = analogRead(POT_PIN);
- Serial.print("PotVal: ");
- Serial.print(PotVal);
- Angle = map(PotVal, 0, 1023, 0, 179);
- Serial.print(", angle: ");
- Serial.println(Angle);
- MyServo.write(Angle);
- delay(15);
+ //PotVal = analogRead(POT_PIN);
+ //Serial.print("PotVal: ");
+ //Serial.print(PotVal);
+ //Angle = map(PotVal, 0, 1023, 0, 179);
+ for (int RotVal = 0; RotVal <= 1023; RotVal++)
+  {
+    Angle = map (RotVal, 0, 1023, 0, 179);
+    Serial.print("Angle: ");
+    Serial.print(Angle);
+    Serial.print("  RotVal: ");
+    Serial.println(RotVal);
+    MyServo.write(Angle);
+    delay(100);  
+  }
+ if (RotVal == 1023)
+  {
+   RotVal = 0; 
+  }
 }
